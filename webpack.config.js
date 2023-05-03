9a97b8c37a9e6853af6736f26c1f400ae0d71717
yongki95 <yongki95@vt.edu>
2023-05-03 16:13:20 -0700

    Changed config for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -25,9 +25,16 @@ module.exports = {
                 use: ['style-loader', 'css-loader'],
             },
             {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource'
+                test: /\.(svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
             },
+            {
+                test: /\.(png|svg)$/i,
+                loader: 'url-loader',
+                options:{
+                    mimetype: 'image/png',
+                }
+            }
         ],
     },
     optimization: {
